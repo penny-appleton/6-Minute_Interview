f69671067def45398d6772a54999ee1581ea7caa
Penny Appleton <penny.appleton@aa.com>
2025-07-09 13:33:44 -0500

    feat: making this a typescript project

---
+++ TypeScript/README.md
@@ -0,0 +1,20 @@
+# 6 Minute Interview - TypeScript
+
+A TypeScript interview exercise project.
+
+## Installation
+
+```bash
+npm install
+```
+
+## Development
+
+```bash
+npm run build
+npm run test
+```
+
+## Project Structure
+
+This project contains TypeScript source files and test files demonstrating TDD practices.
--- TypeScript/jest.config.js
+++ TypeScript/jest.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	verbose: true,
 	collectCoverage: false,
 	preset: 'ts-jest',
-	moduleFileExtensions:  ["ts", "js"],
-	testMatch: ["**/*.test.(ts)"]
+	testEnvironment: 'node',
+	moduleFileExtensions: ["ts", "js"],
+	testMatch: ["**/*.test.ts"],
+	collectCoverageFrom: [
+		"**/*.ts",
+		"!**/*.test.ts",
+		"!**/node_modules/**",
+		"!**/dist/**"
+	]
 };
